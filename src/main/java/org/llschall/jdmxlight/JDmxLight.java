96e0714b2368c0becf7fe756bdf5bbd90fa6417f
llschall <llschall@gmx.com>
2024-05-15 01:19:02 +0200

    [Repo] Use Kotlin for main code

--- src/main/java/org/llschall/jdmxlight/JDmxLight.java
+++
@@ -1,7 +0,0 @@
-package org.llschall.jdmxlight;
-
-public class JDmxLight {
-
-    public static final String VERSION = "0.0.1";
-
-}
---
+++ src/main/kotlin/org/llschall/jdmxlight/JDmxLight.kt
@@ -0,0 +1,5 @@
+package org.llschall.jdmxlight
+
+object JDmxLight {
+    const val VERSION: String = "0.0.1"
+}
