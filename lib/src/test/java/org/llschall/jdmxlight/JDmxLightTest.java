76ad7e35386921a1a53fb98d14dc4d80f947ef51
llschall <llschall@gmx.com>
2024-05-17 23:32:21 +0200

    [Build] Depend on Ardwloop

--- lib/src/test/java/org/llschall/jdmxlight/JDmxLightTest.java
+++ lib/src/test/java/org/llschall/jdmxlight/JDmxLightTest.java
@@ -2,12 +2,14 @@ package org.llschall.jdmxlight;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.llschall.ardwloop.ArdwloopStarter;
 
 public class JDmxLightTest {
 
     @Test
     public void testSetup() {
         Assertions.assertFalse(JDmxLight.VERSION.isBlank());
+        Assertions.assertFalse(ArdwloopStarter.ARDWLOOP_VERSION.isBlank());
     }
 
 }
